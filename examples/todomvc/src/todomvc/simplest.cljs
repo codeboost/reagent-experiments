dfa450116327f79c400f1d832206ff931a1c03be
Florin <florin.braghis@gmail.com>
2022-09-21 22:01:17 +0200

    Simplest - 4

--- examples/todomvc/src/todomvc/simplest.cljs
+++ examples/todomvc/src/todomvc/simplest.cljs
@@ -5,6 +5,17 @@
     [reagent.ratom :as ratom :refer [reaction]]
     [clojure.string :as str]))
 
+(defn component-1* [{:keys [first-name last-name on-fname-change on-lname-change]}]
+  [:div
+   [:h3 (str "Hello, " @first-name " " @last-name)]
+   [:hr]
+   [:input {:type :text
+            :value @first-name
+            :on-change on-fname-change}]
+   [:input {:type :text
+            :value @last-name
+            :on-change on-lname-change}]])
+
 (defn component-1 []
   (let [state (r/atom {:first-name ""
                        :last-name ""})
@@ -15,15 +26,10 @@
         on-fname-change #(set-first-name! (-> % .-target .-value))
         on-lname-change #(set-last-name! (-> % .-target .-value))]
     (fn []
-      [:div
-       [:h3 (str "Hello, " @first-name " " @last-name)]
-       [:hr]
-       [:input {:type :text
-                :value @first-name
-                :on-change on-fname-change}]
-       [:input {:type :text
-                :value @last-name
-                :on-change on-lname-change}]])))
+      [component-1* {:first-name first-name
+                     :last-name last-name
+                     :on-fname-change on-fname-change
+                     :on-lname-change on-lname-change}])))
 
 
 (defn component []
