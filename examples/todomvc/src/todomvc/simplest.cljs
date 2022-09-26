b06b8397029fadad35d838ff34f3e468e4fcd072
Florin <florin.braghis@gmail.com>
2022-09-26 13:49:55 +0200

    Simplest 5 - State adapter

--- examples/todomvc/src/todomvc/simplest.cljs
+++ examples/todomvc/src/todomvc/simplest.cljs
@@ -5,7 +5,21 @@
     [reagent.ratom :as ratom :refer [reaction]]
     [clojure.string :as str]))
 
-(defn component-1* [{:keys [first-name last-name on-fname-change on-lname-change]}]
+
+(defn component-1-state [state]
+  (let [first-name (reaction (:first-name @state))
+        last-name (reaction (:last-name @state))]
+    {:first-name first-name
+     :last-name last-name
+     :set-first-name! #(swap! state assoc :first-name %)
+     :set-last-name! #(swap! state assoc :last-name %)}))
+
+(defn component-1-state-adapter [{:keys [set-first-name! set-last-name!] :as state}]
+  (assoc state :handlers {:on-fname-change #(set-first-name! (-> % .-target .-value))
+                          :on-lname-change #(set-last-name! (-> % .-target .-value))}))
+
+(defn component-1* [{:keys [first-name last-name]
+                     {:keys [on-fname-change on-lname-change]} :handlers}]
   [:div
    [:h3 (str "Hello, " @first-name " " @last-name)]
    [:hr]
@@ -17,19 +31,12 @@
             :on-change on-lname-change}]])
 
 (defn component-1 []
-  (let [state (r/atom {:first-name ""
-                       :last-name ""})
-        first-name (reaction (:first-name @state))
-        last-name (reaction (:last-name @state))
-        set-first-name! #(swap! state assoc :first-name %)
-        set-last-name! #(swap! state assoc :last-name %)
-        on-fname-change #(set-first-name! (-> % .-target .-value))
-        on-lname-change #(set-last-name! (-> % .-target .-value))]
+  (let [state (-> {:first-name "" :last-name ""}
+                  r/atom
+                  component-1-state
+                  component-1-state-adapter)]
     (fn []
-      [component-1* {:first-name first-name
-                     :last-name last-name
-                     :on-fname-change on-fname-change
-                     :on-lname-change on-lname-change}])))
+      [component-1* state])))
 
 
 (defn component []
