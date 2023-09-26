4cc6d1dc9e3cccb0d7c4c63a3c4b0e69c3248d87
Florin Braghis <florin@libertv.ro>
2023-09-26 21:11:04 +0200

    Simple example using VC

--- examples/simple/src/simpleexample/core_v2.cljs
+++ examples/simple/src/simpleexample/core_v2.cljs
@@ -1,66 +1,41 @@
 (ns simpleexample.core-v2
   (:require
     [clojure.string :as str]
-    [reagent.core :as r]))
+    [reagent.core :as r]
+    [reagent.ratom :refer [reaction]]))
 
-(defprotocol IColor
-  (-set-color! [this color])
-  (-color [this]))
-
-(defprotocol IDateTick
-  (-date-string [this])
-  (-start-ticking! [this])
-  (-stop-ticking! [this]))
-
-(deftype ExampleState [state]
-  IColor
-  (-set-color! [this color]
-    (swap! state assoc :color color))
-  (-color [this]
-    (:color @state))
-
-  IDateTick
-  (-date-string [this]
-    (let [date (:date @state)]
-      (-> date .toTimeString (str/split " ") first)))
-
-  (-start-ticking! [this]
-    (swap! state assoc :timer (js/setInterval
-                                #(swap! state assoc :date (js/Date.)) 1000)))
-  (-stop-ticking! [this]
-    (swap! state (fn [{:keys [timer] :as s}]
-                   (when timer
-                     (js/clearInterval timer))
-                   (dissoc s :timer)))))
-
-(defn example-state []
-  (let [state (r/atom {:color "#f34"
-                       :date (js/Date.)})]
-    (ExampleState. state)))
-
-;;;-----------
+(defn greeting-controller []
+  (let [state (r/atom {:timer (js/Date.)
+                       :time-color "#f34"})
+        timer-updater (js/setInterval
+                       #(swap! state assoc :timer (js/Date.)) 1000)]
+    {:time-str (reaction
+                (-> (:timer @state) .toTimeString (str/split " ") first))
+     :time-color (reaction (:time-color @state))
+     :set-time-color! #(swap! state assoc :time-color (-> % .-target .-value))}))
 
 (defn greeting [message]
   [:h1 message])
 
-(defn color-input [es]
+(defn clock [{:keys [time-str time-color]}]
+  [:div.example-clock
+   {:style {:color @time-color}}
+   @time-str])
+
+(defn color-input [{:keys [time-color set-time-color!]}]
   [:div.color-input
    "Time color: "
    [:input {:type "text"
-            :value (-color es)
-            :on-change #(-set-color! es (-> % .-target .-value))}]])
+            :value @time-color
+            :on-change set-time-color!}]])
 
-
-(defn clock [es]
-  (let [time-str (-date-string es)]
-    [:div.example-clock
-     {:style {:color (-color es)}}
-     time-str]))
+(defn simple-example* [controller]
+  [:div
+   [greeting "Hello world, it is now!"]
+   [clock controller]
+   [color-input controller]])
 
 (defn simple-example []
-  (let [es (example-state)]
+  (let [controller (greeting-controller)]
     (fn []
-      [:div
-       [greeting "Hello world, it is now"]
-       [clock es]
-       [color-input es]])))
+      [simple-example* controller])))
